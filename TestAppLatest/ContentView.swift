2a7219188178ef85d2eab60a850d24b16204a8bd
Gabriel den Hartog <g.ghartog@ext.ppay.me>
2025-12-08 17:04:12 -0300

    Initial Commit

---
+++ TestAppLatest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestAppLatest
+//
+//  Created by Gabriel Rapallo Den Hartog on 08/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestAppLatest/TestAppLatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppLatestApp.swift
+//  TestAppLatest
+//
+//  Created by Gabriel Rapallo Den Hartog on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppLatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
